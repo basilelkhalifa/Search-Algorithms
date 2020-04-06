cf5d8dfc8d702ba8da715016fec667bce1f4ff39
basilelkhalifa <basilosama.2000@gmail.com>
2020-04-06 17:55:50 +0300

    Fixed issue with finding goal

--- BlockWorld/breadth_first_search.py
+++ BlockWorld/breadth_first_search.py
@@ -13,11 +13,11 @@ def breadth_first_search_algorithm(initial_state):
     successors = [initial_state]
     state_index = 0
     end = 1
-    while state_index < 60:
+    while state_index < end:
         state = successors[state_index]
         if reached_goal(state):
             print('GOAL FOUND')
-            break
+            return state
         successor_states = find_successors(state)
         for successor_state in successor_states:
             if successor_state in successors:
@@ -25,11 +25,14 @@ def breadth_first_search_algorithm(initial_state):
         successors.extend(successor_states)
         state_index += 1
         end = len(successors)
-    print(successors)
 
 
+state = 'PLEASE ENTER A VALID STATE HERE' \
+        'PLEASE MAKE SURE THAT YOUR STATE HAS THE FOLLOWING BLOCKS:' \
+        '2 GREEN BLOCKS ( ONE VERITCAL AND THE OTHER HORIZONTAL)' \
+        '1 RED BLOCK' \
+        '6 BLUE BLOCKS' \
+        '2 EMPTY BLOCKS '
+' EXAMPLE STATE [ [1,2,1,1], [1,2,4,4], [2,2,4,4], [1,1,0,0] ]'
 
-
-
-
-
+visualise.start_simulation(breadth_first_search_algorithm(state))
